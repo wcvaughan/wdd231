c6523e611db0d03e4a9feed79339fafd2dc6c7f9
wcvaughan <chrisvaughanlds@gmail.com>
2025-03-01 23:54:58 -0900

    start on homepage, basic outline, TODO: fill in html details, stylesheets desktop and mobile

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentyear').textContent = currentYear;
+
+    const lastModifiedDate = document.lastModified;
+    document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`;
+});
