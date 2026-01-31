66f2da9d8c5475d72bc7b936d030612359e77364
Chris <109022755+wcvaughan@users.noreply.github.com>
2026-01-30 21:31:56 -0900

    a

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -23,7 +23,7 @@ document.querySelectorAll('#nav-bar a').forEach(link => {
     const linkPage = link.getAttribute('href');
 
     if (linkPage === currentPage) {
-        link.parentElement.classList.add('active');
+        link.parentElement.classList.add('current');
         link.setAttribute('aria-current', 'page');
     }
 });
